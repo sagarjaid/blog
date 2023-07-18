274eee413f9c29084e10005decf831989236511c
sagarjaid <sagarjaid321@gmail.com>
2023-07-18 23:18:24 +0530

    blog font

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='text-xl m-=auto flex justify-center items-center h-screen'>This is POC blog</div>
+    <div className='text-4xl font-semibold m-=auto flex justify-center items-center h-screen'>This is POC blog</div>
   )
 }
 
