0fbe49b840716192c1a2f8f22e6f3396acc247d9
sagarjaid <69151227+sagarjaid@users.noreply.github.com>
2023-07-20 14:47:17 +0000

    blog page added

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,179 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div className='text-4xl font-semibold m-=auto flex justify-center items-center h-screen'>This is POC blog</div>
-  )
-}
+    <div>
+      <main className="max-w-7xl flex m-auto flex-col p-4 gap-4">
+        <nav className="flex justify-between item-center border-b py-4">
+          <div className="text-3xl font-bold">blogName</div>
+          {/* <img
+            src="https://careerdekho.ai/media/careerdekho-logo.svg"
+            className="w-[180px]"
+          /> */}
+          <div className="flex gap-4 item-center">
+            <ul className="flex flex-row item-center justify-center p-1 gap-4">
+              <li className="cursor-pointer">menu1</li>
+              <li className="cursor-pointer">menu2</li>
+              <li className="cursor-pointer">menu3</li>
+              <li className="cursor-pointer">menu4</li>
+            </ul>
+            <a className="p-1 px-3 cursor-pointer hover:scale-105 border border-black rounded-lg">
+              call-to-action
+            </a>
+          </div>
+        </nav>
+        <div className="flex flex-col gap-2 py-6">
+          <div className="text-3xl">Blog</div>
+          <div className="text-lg">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+          </div>
+        </div>
 
-export default Home
+        <div className="flex flex-col  max-w-4xl gap-4  w-full">
+          <div className="flex gap-4 rounded-xl cursor-pointer border hover:shadow shadow-sm">
+            <img
+              src="https://coolbackgrounds.io/images/backgrounds/black/pure-black-background-f82588d3.jpg"
+              className="w-1/2 rounded-l-xl"
+            />
+            <div className="w-full flex flex-col gap-4 p-4">
+              <div className="text-xl font-bold">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit
+              </div>
+              <div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec
+                erat in turpis tincidunt mollis. Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. in turpis tincidunt mollis...
+              </div>
+              <div className="text-sm">Friday, 12 June 2023</div>
+              <div className="flex items-center gap-2">
+                <img src="/clap-2.png" className="w-12" />
+                <div className="flex items-center gap-2">
+                  <img src="/favicon.ico" className="w-4" />
+                  <div className="text-sm">Sagar Jaid</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col  max-w-4xl gap-4  w-full">
+          <div className="flex gap-4 rounded-xl cursor-pointer border hover:shadow shadow-sm">
+            <img
+              src="https://coolbackgrounds.io/images/backgrounds/black/pure-black-background-f82588d3.jpg"
+              className="w-1/2 rounded-l-xl"
+            />
+            <div className="w-full flex flex-col gap-4 p-4">
+              <div className="text-xl font-bold">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit
+              </div>
+              <div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec
+                erat in turpis tincidunt mollis. Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. in turpis tincidunt mollis...
+              </div>
+              <div className="text-sm">Friday, 12 June 2023</div>
+              <div className="flex items-center gap-2">
+                <img src="/clap-2.png" className="w-12" />
+                <div className="flex items-center gap-2">
+                  <img src="/favicon.ico" className="w-4" />
+                  <div className="text-sm">Sagar Jaid</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col  max-w-4xl gap-4  w-full">
+          <div className="flex gap-4 rounded-xl cursor-pointer border hover:shadow shadow-sm">
+            <img
+              src="https://coolbackgrounds.io/images/backgrounds/black/pure-black-background-f82588d3.jpg"
+              className="w-1/2 rounded-l-xl"
+            />
+            <div className="w-full flex flex-col gap-4 p-4">
+              <div className="text-xl font-bold">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit
+              </div>
+              <div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec
+                erat in turpis tincidunt mollis. Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. in turpis tincidunt mollis...
+              </div>
+              <div className="text-sm">Friday, 12 June 2023</div>
+              <div className="flex items-center gap-2">
+                <img src="/clap-2.png" className="w-12" />
+                <div className="flex items-center gap-2">
+                  <img src="/favicon.ico" className="w-4" />
+                  <div className="text-sm">Sagar Jaid</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col  max-w-4xl gap-4  w-full">
+          <div className="flex gap-4 rounded-xl cursor-pointer border hover:shadow shadow-sm">
+            <img
+              src="https://coolbackgrounds.io/images/backgrounds/black/pure-black-background-f82588d3.jpg"
+              className="w-1/2 rounded-l-xl"
+            />
+            <div className="w-full flex flex-col gap-4 p-4">
+              <div className="text-xl font-bold">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit
+              </div>
+              <div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec
+                erat in turpis tincidunt mollis. Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. in turpis tincidunt mollis...
+              </div>
+              <div className="text-sm">Friday, 12 June 2023</div>
+              <div className="flex items-center gap-2">
+                <img src="/clap-2.png" className="w-12" />
+                <div className="flex items-center gap-2">
+                  <img src="/favicon.ico" className="w-4" />
+                  <div className="text-sm">Sagar Jaid</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-6">
+          <div className="flex justify-between items-center gap-6 pt-8 pr-4 border-t">
+            <div className="w-1/2 flex flex-col gap-2 ">
+              <div className="text-2xl font-bold">blogName</div>
+              <div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec
+                erat in turpis tincidunt mollis. Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit.
+              </div>
+              <div className="flex gap-2 underline cursor-pointer text-sm">
+                <div>LinkedIn</div>
+                <div>Twitter</div>
+              </div>
+            </div>
+            <div className="flex flex-col gap-2 text-sm">
+              <div className="font-bold">Menu</div>
+              <ul className="flex flex-col item-center justify-center gap-2">
+                <li className="cursor-pointer">menu1</li>
+                <li className="cursor-pointer">menu2</li>
+                <li className="cursor-pointer">menu3</li>
+                <li className="cursor-pointer">menu4</li>
+              </ul>
+            </div>
+          </div>
+          <div className="flex items-center justify-between text-xs">
+            <div>
+              © 2023 2022 blog Inc. All rights reserved. Privacy Policy Terms of
+              Use
+            </div>
+            <a className="p-1 px-3 cursor-pointer hover:scale-105 border rounded-lg">
+              made with SaaSblog.ai
+            </a>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
